00b5e1860c0e2bfea840dabdfe556e45989df1aa
kartik894 <f2013037@goa.bits-pilani.ac.in>
2015-02-16 18:30:31 +0530

    Changed to MySqli database
    Changed to mysqli and fetched top 3 results from database.

--- code/submit.php
+++ code/submit.php
@@ -14,15 +14,14 @@
 	$dbhost = '127.0.0.1';
 	$dbuser = 'root';
 	$dbpass = 'password';
-	$dbname = 'MMTP'
-	$conn = mysql_connect($dbhost, $dbuser, $dbpass, $dbname);
-	if(! $conn )
+	$dbname = 'MMTP';
+	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	if(mysqli_connect_errno())
 	{	
-		die('Could not connect: ' . mysql_error());
+		die('Could not connect: ' . mysqli_connect_error());
 	}
-	$sql = 'SELECT * from mytable where src_name=$Source AND dest_name=$Destination AND isAC=$isAC '.
-		'AND isSlpr=$isSleeper;'
-	$retval = mysql_query( $sql, $conn );
+	$sql = 'SELECT * from mytable where src_name=$Source AND dest_name=$Destination AND isAC=$isAC AND isSlpr=$isSleeper;';
+	$retval = mysqli_query($conn, $sql);
 	
 	//Computing upper and lower bounds for fare,travel time and available seats	
 	$maxtime = 0;
@@ -32,25 +31,23 @@
 	$maxSeat = 0;
 	$minSeat = 500000;
 	$maxRating = 0.0;
-	while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+	while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
 	{
 	 	$maxTime = max($maxTime,$row['bus_duration']);
 		$minTime = min($minTime,$row['bus_duration']);
 	
 		$maxFare = max($maxFare,$row['bus_fare']);
 		$minFare = min($minFare,$row['bus_fare']);
-
 		$maxSeat = max($maxSeat,$row['available_seat']);
 		$minSeat = min($minSeat,$row['available_seat']);
 		
 		$maxRating = max($maxRating,$row['bus_rating']);
 	} 
-	
-	//Computing U
-	$sql = 'SELECT * from mytable where src_name=$Source AND dest_name=$Destination AND isAC=$isAC '.
-		'AND isSlpr=$isSleeper;'
-	$retval = mysql_query( $sql, $conn );
-	while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+	$arr=array();
+	//Computing U and adding it to the array, $arr
+	$sql = 'SELECT * from mytable where src_name=$Source AND dest_name=$Destination AND isAC=$isAC AND isSlpr=$isSleeper;';
+	$retval = mysqli_query($conn, $sql);
+	while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
 	{
 	 	$normTime = ($row['bus_duration'] - $maxTime)/($maxTime - $minTime);
 		$normFare = ($row['bus_fare'] - $maxFare)/($maxFare - $minFare);
@@ -58,7 +55,30 @@
 		$normRating = $row['bus_rating']/$maxRating;
 		$U = (11 - $row['bus_duration'])*1000*$normTime + $row['bus_fare']*$normFare + (1/$row['bus_rating']*$normRating);
 		$U += $row['available_seat']*$normSeat;
+		$arr[$row['bus_routeid']]=$U;
 	}
-	mysql_close($conn); 
+
+
+	krsort($arr);
+    	$key = $value = NULL;
+    	$i=0;
+    //Displaying top 3 results
+    	foreach ($arr as $key => $value) {
+        //echo "$key = $value\n";
+    	$sql = 'SELECT operator,src_name,dest_name,bus_starttime,bus_endtime,bus_fare,bus_isAc,bus_isNAc,bus_isSlpr FROM mytable WHERE bus_routeid = $key';
+        $retval = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_array($retval, MYSQLI_ASSOC);
+        echo $row['operator'].'|'.$row['src_name'].'|'.$row['dest_name'].'|'.$row['bus_starttime'].'|'.$row['bus_endtime'].'|'.$row['bus_fare'];
+        if($row['bus_isSlpr']=='true') print 'Sleeper | ';
+        if($row['bus_isNAc']=='true') echo 'Non-AC';
+        else echo 'AC';
+        echo '--------------------------------------------------------------------';
+        $i=$i+1;
+        if($i >= 3)
+            break;
+	}
+	//Freeing result and closing the connection
+	mysqli_free_result($result);
+	mysqli_close($conn);
 ?>
 </html>
