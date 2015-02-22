c0add9abe7e6cd61181ae19c0e70b22ab59e9576
ANADI RASTOGI <anadi.rastogi47@gmail.com>
2015-02-22 20:46:18 +0530

    Prototype Up and running
    mmtp.4rog.in

--- code/submit.php
+++ code/submit.php
@@ -2,37 +2,45 @@
 <h2>Result</h2>
 <?php
 	//Storing the variables passed from main page
-	$Source = $_POST['Source'];
-	$Destination = $_POST['Destination'];
-	$isAC = $_POST['isAC'];
-	$isSleeper = $_POST['isSleeper'];
-	$costScale = (int)$_POST['costScale'];
-	$timeScale = (int)$_POST['timeScale'];
-	$seatScale = (int)$_POST['seatScale'];
+	$Source = $_POST['src'];
+	$Destination = $_POST['dest'];
+	$costScale = (int)$_POST['fare'];
+	$timeScale = (int)$_POST['time'];
+	$seatScale = (int)$_POST['seat'];
+	$isAC='false';
+	printf("%s",$Source);
+	printf("%s",$Destination);
+	if(isset($_POST['isAC']))
+	{
+    $isAC='true';
+    
+	}
 	
+	$isSleeper = 'false';
+	if(isset($_POST['isSleeper']))
+	{
+    $isSleeper='true';
+	}
+	printf("%s",$isAC);
+	printf("%s",$isSleeper);
 	//Connection to database
 	$dbhost = '127.0.0.1';
-	$dbuser = 'root';
-	$dbpass = 'password';
-	$dbname = 'MMTP';
-
-	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	
+	$conn = mysqli_connect("127.0.0.1","root","","buses");
 	if(mysqli_connect_errno())
 	{	
 		die('Could not connect: ' . mysqli_connect_error());
 	}
-
-	$sql = 'SELECT * from mytable where src_name=$Source AND dest_name=$Destination AND isAC=$isAC AND isSlpr=$isSleeper;';
-	$retval = mysqli_query($conn, $sql);
-
+	$sql = "SELECT * from mytable where src_name='$Source' AND dest_name='$Destination' AND bus_isAC='$isAC' AND bus_isSlpr='$isSleeper';";
+	$retval = mysqli_query($conn, $sql) or die ('Error1');
 	//Computing upper and lower bounds for fare,travel time and available seats	
-	$maxtime = 0;
-	$mintime = 500000;
-	$maxFare = 0;
+	$maxTime = 1;
+	$minTime = 500000;
+	$maxFare = 1;
 	$minFare = 500000;
-	$maxSeat = 0;
+	$maxSeat = 1;
 	$minSeat = 500000;
-	$maxRating = 0.0;
+	$maxRating = 1.0;
 	while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
 	{
 	 	$maxTime = max($maxTime,$row['bus_duration']);
@@ -46,10 +54,9 @@
 		$maxRating = max($maxRating,$row['bus_rating']);
 	} 
 	$arr=array();
-
 	//Computing U and adding it to the array, $arr
-	$sql = 'SELECT * from mytable where src_name=$Source AND dest_name=$Destination AND isAC=$isAC AND isSlpr=$isSleeper;';
-	$retval = mysqli_query($conn, $sql);
+	$sql = "SELECT * from mytable where src_name='$Source' AND dest_name='$Destination' AND bus_isAC='$isAC' AND bus_isSlpr='$isSleeper';";
+	$retval = mysqli_query($conn, $sql) or die ('Error2');
 	while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
 	{
 	 	$normTime = ($row['bus_duration'] - $maxTime)/($maxTime - $minTime);
@@ -59,19 +66,16 @@
 		$U = (11 - $row['bus_duration'])*1000*$normTime + $row['bus_fare']*$normFare + (1/$row['bus_rating']*$normRating);
 		$U += $row['available_seat']*$normSeat;
 		$arr[$row['bus_routeid']]=$U;
-
 	}
-
-
 	krsort($arr);
     	$key = $value = NULL;
     	$i=0;
     //Displaying top 3 results
     	foreach ($arr as $key => $value) {
         //echo "$key = $value\n";
-    	$sql = 'SELECT operator,src_name,dest_name,bus_starttime,bus_endtime,bus_fare,bus_isAc,bus_isNAc,bus_isSlpr FROM mytable WHERE bus_routeid = $key';
-        $retval = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_array($retval, MYSQLI_ASSOC);
+    	$sql = "SELECT operator,src_name,dest_name,bus_starttime,bus_endtime,bus_fare,bus_isAc,bus_isNAc,bus_isSlpr FROM mytable WHERE bus_routeid = '$key';";
+        $retval = mysqli_query($conn, $sql) or die ('Error');
+        $row = mysqli_fetch_array($retval, MYSQLI_ASSOC) ;
         echo $row['operator'].'|'.$row['src_name'].'|'.$row['dest_name'].'|'.$row['bus_starttime'].'|'.$row['bus_endtime'].'|'.$row['bus_fare'];
         if($row['bus_isSlpr']=='true') print 'Sleeper | ';
         if($row['bus_isNAc']=='true') echo 'Non-AC';
@@ -82,8 +86,7 @@
             break;
 		}
 	//Freeing result and closing the connection
-	mysqli_free_result($result);
+	mysqli_free_result($retval);
 	mysqli_close($conn);
-
 ?>
 </html>
