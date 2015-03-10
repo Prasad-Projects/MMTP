65738883bc326f664e26d86e67e1c0a4350a5fc6
ANADI RASTOGI <anadi.rastogi47@gmail.com>
2015-03-11 02:01:13 +0530

    Fixed bugs in the submit.php

--- code/submit.php
+++ code/submit.php
@@ -1,5 +1,5 @@
 <html>
-<h2>Result</h2>
+<h1>Result</h1>
 <?php
 //Storing the variables passed from main page
 $Source = $_POST['src'];
@@ -12,8 +12,10 @@ $countBus=0;
 $countAir=0;
 
 $isSleeper='false';
-printf("%s",$Source);
-printf("%s",$Destination);
+echo "<h2>Your Query</h2>";
+echo "<b>Source City : </b>".$Source."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."<b>Destination City : </b>".$Destination.
+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Mode : </b>";
+
 $isFood='false';
 	$isTrain=0;
 	if(isset($_POST['isTrain']))
@@ -26,14 +28,14 @@ $isFood='false';
 	if(isset($_POST['isBus']))
 	{
     $isBus=1;
-    
+    echo " Bus";
 	}
 
 	$isAir=0;
 	if(isset($_POST['isAir']))
 	{
     $isAir=1;
-    
+    echo " Air";
 	}
 
 if(isset($_POST['isFood']))
@@ -50,11 +52,13 @@ if(isset($_POST['isSleeper']))
 {
 $isSleeper='true';
 }
-printf("%s",$isAC);
-printf("%s",$isSleeper);
+
 //Connection to database
 
 if($isBus==1){
+	echo "<br/><br/><div><h2>BUS</h2>";
+	
+
 $dbhost = '127.0.0.1';
 $conn = mysqli_connect("127.0.0.1","root","","buses");
 if(mysqli_connect_errno())
@@ -85,7 +89,7 @@ $arr=array();
 //Computing U and adding it to the array, $arr
 $sql = "SELECT * from mytable where src_name='$Source' AND dest_name='$Destination' AND bus_isAC='$isAC' AND bus_isSlpr='$isSleeper';";
 $retval = mysqli_query($conn, $sql) or die ('Error2');
-$row_cnt = $result->num_rows;
+$row_cnt = $retval->num_rows;
 if($row_cnt==0)
 	echo "No bus route matches the preferences";
 
@@ -95,7 +99,7 @@ $normTime = ($row['bus_duration'] - $maxTime)/($maxTime - $minTime);
 $normFare = ($row['bus_fare'] - $maxFare)/($maxFare - $minFare);
 $normSeat = ($row['bus_duration'] - $maxSeat)/($maxSeat - $minSeat);
 $normRating = $row['bus_rating']/$maxRating;
-$U = (11 - $row['bus_duration'])*1000*$normTime + $row['bus_fare']*$normFare + (1/$row['bus_rating']*$normRating);
+$U = (11 - $row['bus_duration'])*1000*$normTime + $row['bus_fare']*$normFare + (1/($row['bus_rating']*$normRating+0.001));
 $U += $row['available_seat']*$normSeat;
 $arr[$row['bus_routeid']]=$U;
 }
@@ -108,11 +112,12 @@ foreach ($arr as $key => $value) {
 $sql = "SELECT operator,src_name,dest_name,bus_starttime,bus_endtime,bus_fare,bus_isAc,bus_isNAc,bus_isSlpr FROM mytable WHERE bus_routeid = '$key';";
 $retval = mysqli_query($conn, $sql) or die ('Error');
 $row = mysqli_fetch_array($retval, MYSQLI_ASSOC) ;
-echo $row['operator'].'|'.$row['src_name'].'|'.$row['dest_name'].'|'.$row['bus_starttime'].'|'.$row['bus_endtime'].'|'.$row['bus_fare'];
+echo $row['operator'].' | '.$row['src_name'].' | '.$row['dest_name'].' | '.$row['bus_starttime'].' | '.$row['bus_endtime'].' | Rs'.$row['bus_fare']."&nbsp;";
 if($row['bus_isSlpr']=='true') print 'Sleeper | ';
+else print 'Non-Sleeper | ';
 if($row['bus_isNAc']=='true') echo 'Non-AC';
 else echo 'AC';
-echo '--------------------------------------------------------------------';
+echo "<br/>";
 $i=$i+1;
 if($i >= 3)
 break;
@@ -124,8 +129,9 @@ mysqli_close($conn);
 }
 
 if($isAir==1){
+	echo "<br/><br/><h2>AIR</h2>";
 	$dbhost = '127.0.0.1';
-	$conn = mysqli_connect("127.0.0.1","root","","air");
+	$conn = mysqli_connect("127.0.0.1","root","","buses");
 	if(mysqli_connect_errno())
 	{
 	die('Could not connect: ' . mysqli_connect_error());
@@ -144,24 +150,29 @@ while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
 {
 $maxTime = max($maxTime,$row['durationint']);
 $minTime = min($minTime,$row['durationint']);
-$maxFare = max($maxFare,$row['distance']);
-$minFare = min($minFare,$row['distance']);
+$maxFare = max($maxFare,$row['price']);
+$minFare = min($minFare,$row['price']);
 //fare directly proportional to distancee
 }
 $arr=array();
 //Computing U and adding it to the array, $arr
+if($isFood=='true')
+{
 $sql = "SELECT * from airline where src='$Source' AND dest='$Destination' AND is_food='$isFood';";
+}
+else
+$sql = "SELECT * from airline where src='$Source' AND dest='$Destination'";
 $retval = mysqli_query($conn, $sql) or die ('Error2');
-$row_cnt = $result->num_rows;
+$row_cnt = $retval->num_rows;
 if($row_cnt==0)
 	echo "No air route matches the preferences";
 
 while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC))
 {
 $normTime = ($row['durationint'] - $maxTime)/($maxTime - $minTime);
-$normFare = ($row['distance'] - $maxFare)/($maxFare - $minFare);
+$normFare = ($row['price'] - $maxFare)/($maxFare - $minFare + 0.0001);
 
-$U = (11 - $row['durationint'])*1000*$normTime + $row['distance']*$normFare;
+$U = (11 - $row['durationint'])*1000*$normTime + $row['price']*$normFare;
 $arr[$row['sys_id']]=$U;
 }
 krsort($arr);
@@ -171,13 +182,13 @@ $i=0;
 foreach ($arr as $key => $value) {
 //echo "$key = $value\n";
 
-$sql = "SELECT airl,a_id,src,dest,start,end,durationint,is_food FROM mytable WHERE sys_id = '$key';";
+$sql = "SELECT airl,a_id,src,dest,start,end,duration,durationint,is_food,price FROM airline WHERE sys_id = '$key';";
 $retval = mysqli_query($conn, $sql) or die ('Error');
 $row = mysqli_fetch_array($retval, MYSQLI_ASSOC) ;
 
-echo $row['airl'].'|'.$row['a_id'].'|'.$row['src'].'|'.$row['dest'].'|'.$row['start'].'|'.$row['end'].'|'.$row['duration'];
+echo $row['airl'].'|'.$row['a_id'].'|'.$row['src'].'|'.$row['dest'].'|'.$row['start'].'|'.$row['end'].'|'.$row['duration'].'|'.$row['price'];
 
-echo '--------------------------------------------------------------------';
+echo "<br/>";
 $i=$i+1;
 if($i >= 3)
 break;
